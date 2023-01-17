9a9dac79dea458e285f8c26f8c4f657c8c63edd4
Darren Jones <1425808+ddaddy@users.noreply.github.com>
2023-01-17 10:48:05 +0100

    Add Usage to results (not available when streaming)

--- Sources/OpenAISwift/Models/OpenAI.swift
+++ Sources/OpenAISwift/Models/OpenAI.swift
@@ -8,9 +8,16 @@ public struct OpenAI: Codable {
     public let object: String
     public let model: String?
     public let choices: [Choice]
+    public let usage: Usage?
 }
 
 public struct Choice: Codable {
     public let text: String
     public let finish_reason: String?
+}
+
+public struct Usage: Codable {
+    public let prompt_tokens: Int
+    public let completion_tokens: Int
+    public let total_tokens: Int
 }
